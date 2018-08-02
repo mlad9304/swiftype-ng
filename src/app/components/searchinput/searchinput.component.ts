b4a8118da39314b99fe93180d2e43284a87e9a76
rndev80 <jianmyphy123@gmail.com>
2018-08-02 07:52:09 -0700

    search input with dropbox

--- src/app/components/searchinput/searchinput.component.ts
+++ src/app/components/searchinput/searchinput.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
 
+declare var jquery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-searchinput',
   templateUrl: './searchinput.component.html',
@@ -9,6 +12,15 @@ import { SharedService } from '../../services/shared.service';
 export class SearchinputComponent implements OnInit {
 
   query: string = "";
+  activeItemIndex: number = -1;
+  items: string[] = [
+    'ebay',
+    'espn',
+    'expedia',
+    'etsy',
+    'ebates',
+    'enterprise'
+  ];
 
   constructor(
     private sharedService: SharedService
@@ -26,6 +38,66 @@ export class SearchinputComponent implements OnInit {
   input(e) {
     this.query = e.target.value;
     this.sharedService.changedQueryEmitter(this.query);
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(e: KeyboardEvent) {
+    switch(e.key) {
+      case 'ArrowUp':
+        this.selectPreviousItem();
+        break;
+      case 'ArrowDown':
+        this.selectNextItem();
+        break;
+      case 'Enter':
+
+        break;
+    }
+  }
+
+  selectPreviousItem() {
+    let prevIndex = this.activeItemIndex - 1;
+    if(prevIndex >= -1) {
+      this.setActive(prevIndex);
+    } else {
+      this.setActive(this.items.length - 1);
+    }
+  }
+
+  selectNextItem() {
+    let nextIndex = this.activeItemIndex + 1;
+    if(nextIndex < this.items.length) {
+      this.setActive(nextIndex);
+    } else {
+      this.setActive(0);
+    }
+  }
+
+  setActive(index) {
+
+    this.activeItemIndex = index;
+    $('.dropdown-box .dropdown-item').removeClass('active');
+
+    if(index > -1) {      
+      $($('.dropdown-box .dropdown-item')[this.activeItemIndex]).addClass('active');
+      this.query = this.items[index];
+    }
+    
+  }
+
+  render(text) {
+    let element = document.createElement("span");
+    if(this.query == '' || text.indexOf(this.query) == -1) {
+      element.innerHTML = text;
+      return element.outerHTML;
+    }
+
+    element.innerHTML = this.query;
+    let boldElement = document.createElement('b');
+    boldElement.innerHTML = text.substring(text.indexOf(this.query) + this.query.length);
+    element.appendChild(boldElement);
+    return element.outerHTML;
+      
   }
 
 }
